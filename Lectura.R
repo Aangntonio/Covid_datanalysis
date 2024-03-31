2ce39104b78313f371a5adc8f2244a2f88d5d57e
Aangtonio <aang@ciencias.unam.mx>
2024-03-31 17:09:39 -0600

    si

--- Lectura.R
+++ Lectura.R
@@ -6,3 +6,15 @@
 
 data <-  read.csv("Covid_cdmx.csv", header = T)
 View(data)
+defunciones <- subset.data.frame(data, data$FECHA_DEF != '9999-99-99')
+intubados <- subset.data.frame(data, data$INTUBADO == 1 )
+#PArticiones
+
+View(intubados)
+summary(intubados$EDAD)
+
+summary(defunciones$EDAD)
+sd(defunciones$EDAD)
+hist(defunciones$EDAD, breaks = 20, col = "skyblue", xlab = "Edades", ylab = "Frecuencia", main = "Defunciones por Covid en 2023 por edades")
+
+hist(intubados$EDAD, breaks = 20, col = "skyblue", xlab = "Edades", ylab = "Frecuencia", main = "Intubados por Covid en 2023 por edades")
